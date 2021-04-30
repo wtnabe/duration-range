d50abe14fcd49548b39d103290f8af9de6dc5bdf
wtnabe <wtnabe@gmail.com>
2021-04-30 20:27:42 +0900

    separate common functions to module

---
+++ lib/duration_range/common_functions.rb
@@ -0,0 +1,58 @@
+module DurationRange
+  module CommonFunctions
+    #
+    # @param reference_date [Date]
+    # @return [Range]
+    #
+    def this_month(reference_date: today)
+      begin_date = reference_date.at_beginning_of_month
+      end_date = reference_date.at_end_of_month
+    
+      (begin_date..end_date).lazy
+    end
+
+    #
+    # @param count [Integer]
+    # @return [Range]
+    #
+    def next_month(count: 1)
+      this_month(reference_date: today.months_since(count))
+    end
+
+    #
+    # @param count [Integer]
+    # @return [Range]
+    #
+    def last_month(count: 1)
+      this_month(reference_date: today.months_ago(count))
+    end
+ 
+    #
+    # @param reference_date [Date]
+    # @return [Range]
+    #
+    # :reek:UtilityFuncion
+    def this_week(reference_date: today)
+      begin_date = reference_date.beginning_of_week
+      end_date = reference_date.end_of_week
+
+      (begin_date..end_date).lazy
+    end
+
+    #
+    # @param count [Integer]
+    # @return [Range]
+    #
+    def next_week(count: 1)
+      this_week(reference_date: today.weeks_since(count))
+    end
+
+    #
+    # @param count [Integer]
+    # @return [Range]
+    #
+    def last_week(count: 1)
+      this_week(reference_date: today.weeks_ago(count))
+    end
+  end
+end
