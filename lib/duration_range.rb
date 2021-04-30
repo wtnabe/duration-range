991c71a9c8edc47bf0387612ec1e8a43df8f620a
wtnabe <wtnabe@gmail.com>
2021-04-30 18:39:19 +0900

    implement

--- lib/duration_range.rb
+++ lib/duration_range.rb
@@ -1,7 +1,67 @@
 require "duration_range/version"
+require "active_support/core_ext/date"
+require "active_support/core_ext/time"
 
 class DurationRange
   class Error < StandardError; end
-  # Your code goes here...
-end
 
+  def initialize(today: Date.today)
+    @today = today
+  end
+  attr_reader :today
+
+  #
+  # @param reference_date [Date]
+  # @return [Range]
+  #
+  def this_month(reference_date: today)
+    begin_date = reference_date.at_beginning_of_month
+    end_date = reference_date.at_end_of_month
+    
+    (begin_date..end_date).lazy
+  end
+
+  #
+  # @param count [Integer]
+  # @return [Range]
+  #
+  def next_month(count: 1)
+    this_month(reference_date: today.months_since(count))
+  end
+
+  #
+  # @param count [Integer]
+  # @return [Range]
+  #
+  def last_month(count: 1)
+    this_month(reference_date: today.months_ago(count))
+  end
+ 
+  #
+  # @param reference_date [Date]
+  # @return [Range]
+  #
+  # :reek:UtilityFuncion
+  def this_week(reference_date: today)
+    begin_date = reference_date.beginning_of_week
+    end_date = reference_date.end_of_week
+
+    (begin_date..end_date).lazy
+  end
+
+  #
+  # @param count [Integer]
+  # @return [Range]
+  #
+  def next_week(count: 1)
+    this_week(reference_date: today.weeks_since(count))
+  end
+
+  #
+  # @param count [Integer]
+  # @return [Range]
+  #
+  def last_week(count: 1)
+    this_week(reference_date: today.weeks_ago(count))
+  end
+end
--- spec/duration_range_spec.rb
+++ spec/duration_range_spec.rb
@@ -1,9 +1,165 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe DurationRange do
-  it {
-    assert {
-      DurationRange
-    }
-  }
+  #
+  # @param range [Range]
+  # @return [Array]
+  #
+  # :reek:UtilityFunction
+  def range_to_array_of_string(range)
+    arr = range.to_a
+    [arr.first.to_s, arr.last.to_s]
+  end
+
+  describe '#this_month' do
+    describe 'given 2021-04-30' do
+      it 'from 2021-04-01 to 2021-04-30' do
+        duration = DurationRange.new(today: Date.parse('2021-04-30'))
+
+        assert {
+          ['2021-04-01', '2021-04-30'] == range_to_array_of_string(duration.this_month)
+        }
+      end
+    end
+
+    describe 'given 2021-04-20' do
+      it 'from 2021-04-01 to 2021-04-30' do
+        duration = DurationRange.new(today: Date.parse('2021-04-20'))
+
+        assert {
+          ['2021-04-01', '2021-04-30'] == range_to_array_of_string(duration.this_month)
+        }
+      end
+    end
+  end
+
+  describe '#next_month' do
+    describe 'given 2021-04-30' do
+      it 'from 2021-05-01 to 2021-05-31' do
+        duration = DurationRange.new(today: Date.parse('2021-04-30'))
+
+        assert {
+          ['2021-05-01', '2021-05-31'] == range_to_array_of_string(duration.next_month)
+        }
+      end
+    end
+
+    describe 'given 2021-04-30 and 2 month later' do
+      it 'from 2021-06-01 to 2021-06-30' do
+        duration = DurationRange.new(today: Date.parse('2021-04-30'))
+
+        assert {
+          ['2021-06-01', '2021-06-30'] == range_to_array_of_string(duration.next_month(count: 2))
+        }
+      end
+    end
+  end
+
+  describe '#last_month' do
+    describe 'given 2021-04-30' do
+      it 'from 2021-03-01 to 2021-03-31' do
+        duration = DurationRange.new(today: Date.parse('2021-04-30'))
+
+        assert {
+          ['2021-03-01', '2021-03-31'] == range_to_array_of_string(duration.last_month)
+        }
+      end
+    end
+
+    describe 'given 2021-04-30 and 2 months earlier' do
+      it 'from 2021-02-01 to 2021-02-28' do
+        duration = DurationRange.new(today: Date.parse('2021-04-30'))
+
+        assert {
+          ['2021-02-01', '2021-02-28'] == range_to_array_of_string(duration.last_month(count: 2))
+        }
+      end
+    end
+  end
+  
+  describe '#this_week' do
+    describe 'given 2021-04-30' do
+      it 'from 2021-04-26 to 2021-05-02' do
+        duration = DurationRange.new(today: Date.parse('2021-04-30'))
+
+        assert {
+          ['2021-04-26', '2021-05-02'] == range_to_array_of_string(duration.this_week)
+        }
+      end
+    end
+
+    describe 'given 2021-04-20' do
+      it 'from 2021-04-19 to 2021-04-25' do
+        duration = DurationRange.new(today: Date.parse('2021-04-20'))
+
+        assert {
+          ['2021-04-19', '2021-04-25'] == range_to_array_of_string(duration.this_week)
+        }
+      end
+    end
+  end
+
+  describe '#next_week' do
+    describe 'given 2021-04-30' do
+      it 'from 2021-05-03 to 2021-05-09' do
+        duration = DurationRange.new(today: Date.parse('2021-04-30'))
+
+        assert {
+          ['2021-05-03', '2021-05-09'] == range_to_array_of_string(duration.next_week)
+        }
+      end
+    end
+
+    describe 'given 2021-04-20' do
+      it 'from 2021-04-26 to 2021-05-02' do
+        duration = DurationRange.new(today: Date.parse('2021-04-20'))
+
+        assert {
+          ['2021-04-26', '2021-05-02'] == range_to_array_of_string(duration.next_week)
+        }
+      end
+    end
+
+    describe 'given 2021-04-30 and 2 weeks later' do
+      it 'from 2021-05-10 to 2021-05-16' do
+        duration = DurationRange.new(today: Date.parse('2021-04-30'))
+
+        assert {
+          ['2021-05-10', '2021-05-16'] == range_to_array_of_string(duration.next_week(count: 2))
+        }
+      end
+    end
+  end
+
+  describe '#last_week' do
+    describe 'given 2021-04-30' do
+      it 'from 2021-04-19 to 2021-04-25' do
+        duration = DurationRange.new(today: Date.parse('2021-04-30'))
+
+        assert {
+          ['2021-04-19', '2021-04-25'] == range_to_array_of_string(duration.last_week)
+        }
+      end
+    end
+
+    describe 'given 2021-04-20' do
+      it 'from 2021-04-12 to 2021-04-18' do
+        duration = DurationRange.new(today: Date.parse('2021-04-20'))
+
+        assert {
+          ['2021-04-12', '2021-04-18'] == range_to_array_of_string(duration.last_week)
+        }
+      end
+    end
+
+    describe 'given 2021-04-30 and 2 weeks earlier' do
+      it 'from 2021-04-12 to 2021-04-18' do
+        duration = DurationRange.new(today: Date.parse('2021-04-30'))
+
+        assert {
+          ['2021-04-12', '2021-04-18'] == range_to_array_of_string(duration.last_week(count: 2))
+        }
+      end
+    end
+  end
 end
